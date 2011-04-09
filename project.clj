313e3e2a955ad53f214434ad02dc0ba4a7966bfc
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-08 20:30:03 -0400

    Add swank-clojure as dev dep

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [necessary-evil "1.1.0"]
                  [com.miglayout/miglayout "3.7.4"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]]
   :main robot-remote-eval.core)
